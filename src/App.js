ba611bc8203c9fd781c75b35fcdc9040bc8b30e6
Aline Vignoli <nyhv.contato@gmail.com>
2020-10-21 14:28:11 -0300

    tentativa de resolucao de conflito no app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import './components/Filter';
-import Filter from './components/Filter';
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
-        <Filter/>
       </div>
     );
   }
