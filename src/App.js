04f126d98748a7b4ac54a5d2e9c9d4c4df52df8d
Nicole <nzolnier@gmail.com>
2020-10-21 14:28:23 -0300

    atualizacoes pra evitar conflito

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import styled from 'styled-components';
-import Card from './components/Card';
 
-export class App extends React.Component {
-  render(){
+
+class App extends React.Component {
+
+  render() {
     return (
-      <div>
-        <Card id={1}
-        image={"https://picsum.photos/300/300"}
-        name={"Camiseta"}
-        price={0.50} />
+      <div className="App">
       </div>
     );
   }
