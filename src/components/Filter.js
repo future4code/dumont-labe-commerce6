16ccd43e1a013d7689f3cc09a7b7246436d1b628
Nicole <nzolnier@gmail.com>
2020-10-21 19:42:45 -0300

    adicionando frustacoes, nos tentamos aline :(

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -13,22 +13,13 @@ const FilterContainer = styled.div`
 `
 
 class Filter extends React.Component {
-    state = {
-        inputMinValue: "",
-        inputMaxValue: "",
-        inputSearchProduct: ""
-    }
-
-    onChangeInputMinValue = (event) => {
-        this.setState({inputMinValue: event.target.value})
-    }
     
-    onChangeInputMaxValue = (event) => {
-        this.setState({inputMaxValue: event.target.value})
-    }
+    
 
-    onChangeInputSearchProduct = (event) => {
-        this.setState({inputSearchProduct: event.target.value})
+    
+
+    filterByName = () => {
+
     }
 
     render() {
@@ -37,18 +28,18 @@ class Filter extends React.Component {
                 <h2>Filtros:</h2>
                 <p>Valor Mínimo:</p>
                 <input 
-                value={this.state.inputMinValue}
-                onChange={this.onChangeInputMinValue}
-                />
+                value={this.props.inputMinValue}
+                
+                type="number"/>
                 <p>Valor Máximo:</p>
                 <input 
-                value={this.state.inputMaxValue}
-                onChange={this.onChangeInputMaxValue}
-                />
+                value={this.props.inputMaxValue}
+                 
+                type="number"/>
                 <p>Buscar Produto:</p>
                 <input 
-                value={this.state.inputSearchProduct}
-                onChange={this.onChangeInputSearchProduct}
+                value={this.props.inputSearchProduct}
+                
                 />
             </FilterContainer>  
         ) 
