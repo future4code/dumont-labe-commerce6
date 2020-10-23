f38c231323eb51aae6dcf88ce63e1bc183cb568b
Nicole <nzolnier@gmail.com>
2020-10-23 11:01:51 -0300

    ajustando imports

---
+++ src/components/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+export class AboutUs extends React.Component {
+    render() {
+        return (<div>
+        </div>
+        )
+    }
+}
+
+export default AboutUs;
